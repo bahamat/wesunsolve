bb7ee9e2bf43358fda6e595c4ec8657b16fc04db
Thomas Gouverneur <tgo@ians.be>
2011-08-08 12:47:13 +0200

    tgo: Added little function to error msg and exit. tgo: fixed die() calls in www page on -dev branch, now calling       HTTP::errWWW() to cleanly exit;

--- libs/http.obj.php
+++ libs/http.obj.php
@@ -35,6 +35,24 @@ class HTTP
     die();
   }
 
+  public static function errWWW($e) {
+    $index = new Template("./tpl/index.tpl");
+    $head = new Template("./tpl/head.tpl");
+    $menu = new Template("./tpl/menu.tpl");
+    $foot = new Template("./tpl/foot.tpl");
+    $foot->set("start_time", $start_time);
+    $content = new Template("./tpl/error.tpl");
+    $content->set("error", $e);
+
+    $index->set("head", $head);
+    $index->set("menu", $menu);
+    $index->set("content", $content);
+    $index->set("foot", $foot);
+    echo $index->fetch();
+    die();
+  }
+
+
   public static function Piwik($title) {
     global $config;
     @require_once('../../../libs/PiwikTracker.php');
