22ecbf0c1d508e6fec3486b47a21ee9f60ecec98
Thomas Gouverneur <thomas@espix.org>
2012-02-07 19:00:38 +0100

    tgo: Added tool to display mailling lists

--- bin/mstats.sh
+++ bin/mstats.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo awffull --use_geoip -p -c /srv/sunsolve/etc/awffull.conf -o /srv/sunsolve/www/stats /var/log/apache2/access.log
-sudo /usr/sbin/logrotate -fs /srv/sunsolve/etc/logrotate.state /srv/sunsolve/etc/logrotate.conf
+sudo awffull --use_geoip -p -c /srv/sunsolve/etc/awffull.conf -o /srv/sunsolve/www/static/stats ${1}
